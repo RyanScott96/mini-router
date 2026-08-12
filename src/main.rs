ba7e47751a5006972ebad59e9d7ea1e4261cc53d
Ryan Scott <r.l.scott2014@gmail.com>
2026-08-12 14:47:12 -0400

    add global camera2d

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,11 @@ pub mod sync;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
         .add_plugins(RoutingPlugin)
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
 }
